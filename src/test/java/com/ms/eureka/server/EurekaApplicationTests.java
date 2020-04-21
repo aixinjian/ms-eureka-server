e7be22854f3aefd9491d3c04665b7e2b992e77f3
xinjian.ai@hosjoy.com <xinjian.ai@hosjoy.com>
2020-04-21 16:04:04 +0800

    eureka server init

---
+++ src/test/java/com/ms/eureka/server/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
